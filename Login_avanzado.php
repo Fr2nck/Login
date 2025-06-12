837c051e917ddb9261f17cd683f9e41add606b08
Fr2nck <202012672i@utea.edu.pe>
2025-06-12 10:00:24 -0500

    commit

--- Login_avanzado.php
+++ Login_avanzado.php
@@ -7,19 +7,18 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container-fluid">           <!-- contenedor -->
-        <div class="row">                   <!-- fila -->
-            <div class="col-4">             <!-- columna -->
+    <div class="container-fluid">           
+        <div class="row py-2">
+            <div class="col-12 col-md-4 col-lg-3">
                 <div class="card">
                     <div class="card-header bg-primary text-black">    
                         Contenedor 01
                     </div>
                     <div class="card-body">
-                        <img src="..." class="card-img-top" alt="...">
                     </div>
                 </div>
             </div>    
-            <div class="col-4">
+            <div class="col-12 col-md-4 col-lg-3">
                 <div class="card">
                     <div class="card-header bg-info text-black">    
                         Contenedor 02
@@ -28,7 +27,7 @@
                     </div>
                 </div>
             </div>    
-            <div class="col-4">
+            <div class="col-12 col-md-4 col-lg-3">
                 <div class="card">
                     <div class="card-header bg-success text-black">    
                         Contenedor 03
@@ -37,7 +36,7 @@
                     </div>
                 </div>                
             </div>
-    </div>
+        </div>
     <div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </body>
