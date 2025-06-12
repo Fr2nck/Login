bd8dea10c34aef4b0fa412aef8dc8f8249f7a104
Fr2nck <202012672i@utea.edu.pe>
2025-06-12 09:47:58 -0500

    commit

---
+++ Login_avanzado.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Avanzado</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container-fluid">           <!-- contenedor -->
+        <div class="row">                   <!-- fila -->
+            <div class="col-4">             <!-- columna -->
+                <div class="card">
+                    <div class="card-header bg-primary text-black">    
+                        Contenedor 01
+                    </div>
+                    <div class="card-body">
+                        <img src="..." class="card-img-top" alt="...">
+                    </div>
+                </div>
+            </div>    
+            <div class="col-4">
+                <div class="card">
+                    <div class="card-header bg-info text-black">    
+                        Contenedor 02
+                    </div>
+                    <div class="card-body">
+                    </div>
+                </div>
+            </div>    
+            <div class="col-4">
+                <div class="card">
+                    <div class="card-header bg-success text-black">    
+                        Contenedor 03
+                    </div>
+                    <div class="card-body">
+                    </div>
+                </div>                
+            </div>
+    </div>
+    <div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</body>
+</html>
+
