3695e0d99f194df3da1f3e4287fa458d422c2d99
Fr2nck <202012672i@utea.edu.pe>
2025-06-12 10:22:38 -0500

    commit

--- Login_avanzado.php
+++ Login_avanzado.php
@@ -9,7 +9,7 @@
 <body>
     <div class="container-fluid">           
         <div class="row py-2">
-            <div class="col-12 col-md-4 col-lg-3">
+            <div class="col-12 col-md-4 col-lg-4">
                 <div class="card">
                     <div class="card-header bg-primary text-black">    
                         Contenedor 01
@@ -18,16 +18,29 @@
                     </div>
                 </div>
             </div>    
-            <div class="col-12 col-md-4 col-lg-3">
+            <div class="col-12 col-md-4 col-lg-4">
                 <div class="card">
-                    <div class="card-header bg-info text-black">    
-                        Contenedor 02
+                    <div class="card-header bg-info text-black text-center">    
+                        LOGIN DE ACCESO
                     </div>
                     <div class="card-body">
+                        <form action="funciones/PHP_login_basico.php" method="POST">
+                            <div class="text-center py-2">
+                                <label>Usuario: </label>
+                                <input type="text" name="user" class="form-control" placeholder="Usuario" required>
+                            </div>
+                            <div class="text-center py-2">
+                                <label>Contraseña: </label>
+                                <input type="password" name="pass" class="form-control" placeholder="Contraseña" required>
+                            </div>
+                            <div class="text-center py-2">
+                                <button type="submit" class="btn btn-info">Iniciar Sesion</button>
+                            </div>                            
+                        </form>
                     </div>
                 </div>
             </div>    
-            <div class="col-12 col-md-4 col-lg-3">
+            <div class="col-12 col-md-4 col-lg-4">
                 <div class="card">
                     <div class="card-header bg-success text-black">    
                         Contenedor 03
@@ -39,6 +52,7 @@
         </div>
     <div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></scrip>
 </body>
 </html>
 
